052dd80c34e9a93af69167320eb102eec1b52ec0
Cadel Watson <cadel@cadelwatson.com>
2015-12-11 16:20:16 +1100

    Add in-program help strings for all commands

--- main.py
+++ main.py
@@ -7,48 +7,102 @@ class Lexeme(cmd.Cmd):
     intro = "Welcome to Lexeme! Input '?' for help and commands."
     prompt = "\nEnter command: "
 
+    def getHelp(self, command):
+        with open("help.txt", "r") as f:
+            line = f.readline().rstrip()
+            while line != command:
+                line = f.readline().rstrip()
+            f.readline()
+            while line != "====":
+                print(line)
+                line = f.readline().rstrip()
+
     def do_list(self, arg):
         clearScreen()
         listwords()
 
+    def help_list(self):
+        clearScreen()
+        self.getHelp("LIST")
+
     def do_quit(self, arg):
         quit()
+
+    def help_quit(self):
+        clearScreen()
+        self.getHelp("QUIT")
 
     def do_add(self, arg):
         clearScreen()
         add()
 
+    def help_add(self):
+        clearScreen()
+        self.getHelp("ADD")
+
     def do_decline(self, arg):
         clearScreen()
         decline()
+
+    def help_decline(self):
+        clearScreen()
+        self.getHelp("DECLINE")
 
     def do_statistics(self, arg):
         clearScreen()
         statistics()
 
+    def help_statistics(self):
+        clearScreen()
+        self.getHelp("STATISTICS")
+
     def do_search(self, arg):
         clearScreen()
         search()
+
+    def help_search(self):
+        clearScreen()
+        self.getHelp("SEARCH")
 
     def do_generate(self, arg):
         clearScreen()
         generate()
 
+    def help_generate(self, arg):
+        clearScreen()
+        self.getHelp("GENERATE")
+
     def do_export(self, arg):
         clearScreen()
         export()
+
+    def help_export(self, arg):
+        clearScreen()
+        self.getHelp("EXPORT")
 
     def do_exportwords(self, arg):
         clearScreen()
         exportText()
 
+    def help_exportwords(self):
+        clearScreen()
+        self.getHelp("EXPORTWORDS")
+
     def do_batch(self, arg):
         clearScreen()
         batchgenerate()
 
+    def help_batch(self):
+        clearScreen()
+        self.getHelp("BATCH")
+
     def do_import(self, arg):
         clearScreen()
         importWords()
+
+    def help_import(self):
+        clearScreen()
+        self.getHelp("IMPORT")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
